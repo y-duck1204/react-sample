adaf1d6354244f76696fc4f131811ceae734ab71
kmst <y.duck.3595@gmail.com>
2024-06-15 23:24:32 +0900

    チュートリアル途中まで

--- tutorial/src/App.js
+++ tutorial/src/App.js
@@ -1,3 +1,28 @@
-export default function Square() {
-  return <button className="square">X</button>;
+import { useState } from "react";
+
+function Squeare({ value }) {
+  return <button className="squeare">{value}</button>;
+}
+
+export default function Board() {
+  const [squeares, setSqueares] = useState(Array(9).fill(null));
+  return (
+    <>
+      <div className="board-row">
+        <Squeare value={squeares[0]} />
+        <Squeare value={squeares[1]} />
+        <Squeare value={squeares[2]} />
+      </div>
+      <div className="board-row">
+        <Squeare value={squeares[3]} />
+        <Squeare value={squeares[4]} />
+        <Squeare value={squeares[5]} />
+      </div>
+      <div className="board-row">
+        <Squeare value={squeares[6]} />
+        <Squeare value={squeares[7]} />
+        <Squeare value={squeares[8]} />
+      </div>
+    </>
+  );
 }
